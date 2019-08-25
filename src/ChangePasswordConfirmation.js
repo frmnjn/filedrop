39b012aafd4fe83d6b4693823d10d5ef52fbd32d
Firman Budi Safrizal <firmanbudis@gmail.com>
2019-08-25 15:12:55 +0700

    import auth amplify changepasswordconfirmation

--- src/ChangePasswordConfirmation.js
+++ src/ChangePasswordConfirmation.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { Auth } from "aws-amplify";
 import "./App.css";
 
 class ChangePasswordConfirmation extends Component {
